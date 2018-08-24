b0c01ed7c64b4d236e626c9d56787d166346714d
nour aldeen <nour.aldeen11@outlook.com>
2018-08-24 12:27:00 +0200

    backend dooooooooooooooooooone

--- Lexicon LMS/Lexicon LMS/Models/Course.cs
+++ Lexicon LMS/Lexicon LMS/Models/Course.cs
@@ -11,7 +11,7 @@ namespace Lexicon_LMS.Models
         public int Id { get; set; }
         [Display(Name = "Start Time")]
         public DateTime StartTime { get; set; }
-        [Display(Name = "Start Time")]
+        [Display(Name = "End Time")]
         public DateTime EndTime { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
